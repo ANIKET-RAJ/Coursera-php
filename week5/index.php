48883d7d2aa20f51cae022317102895c82d16cc8
ANIKET-RAJ <aniketraj4u@gmail.com>
2018-03-06 17:48:45 +0530

    week5

---
+++ week5/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Aniket Raj PHP</title>
+</head>
+<body>
+<h1>Aniket Raj learning PHP</h1>
+
+<p>The SHA256 hash of "Aniket Raj" is
+<?php print hash('sha256','aniketraj') ?></p>
+
+
+<pre>ASCII ART:
+
+	      **    
+	     ****   
+	    **  **  
+	   **    ** 
+	  **********
+	 **        **
+	**          **
+
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
